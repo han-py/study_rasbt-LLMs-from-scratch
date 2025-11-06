4b9cfb05b98029c09a9899fcc013bfdc21ef969b
HaPpY <930333105@qq.com>
2025-11-06 11:56:27 +0800

    feat(tokenizer): 实现能够处理未知单词的文本分词器
    - 添加 SimpleTokenizerV2 类以支持未知词元处理
    - 在编码过程中使用 <|unk|> 替换未知单词
    - 更新正则表达式预处理逻辑以提高分词准确性-优化解码函数中的空格和标点符号处理
    - 保留原有注释结构并增强代码可读性

--- U2/01_main-chapter-code/U2.py
+++ U2/01_main-chapter-code/U2.py
@@ -97,3 +97,31 @@ class SimpleTokenizerV1:
 # # 如果有训练集之外的新样本，则会报错 KeyError: 'Hello'
 # text = "Hello, do you like tea?"
 # print(tokenizer.encode(text))
+
+
+# 代码清单2-4 能够处理未知单词的文本分词器
+class SimpleTokenizerV2:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i: s for s, i in vocab.items()}
+
+    def encode(self, text):
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+        preprocessed = [
+            item.strip() for item in preprocessed if item.strip()
+        ]
+
+        # 用<|unk|>词元替换未知单词
+        preprocessed = [
+            item if item in self.str_to_int
+            else "<|unk|>"
+            for item in preprocessed
+        ]
+
+        ids = [self.str_to_int[token] for token in preprocessed]
+        return ids
+
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        return text
