017c2bba530de7f9c5453fe35a04c634fa428819
HaPpY <930333105@qq.com>
2025-11-06 11:09:13 +0800

    feat(U2): 实现词汇表创建功能
    - 添加numpy导入以支持整数类型
    - 注释掉原有的打印语句
    - 引入集合排序逻辑确定唯一单词数量
    - 实现从预处理文本到词汇表的映射
    - 添加词汇表内容展示的注释代码

--- U2/01_main-chapter-code/U2.py
+++ U2/01_main-chapter-code/U2.py
@@ -1,11 +1,14 @@
 import urllib.request
 import re
 
+from numpy import integer
+
 # url = ("https://raw.githubusercontent.com/rasbt/"
 #        "LLMs-from-scratch/main/ch02/01_main-chapter-code/"
 #        "the-verdict.txt")
 # file_path = "the-verdict.txt"
 # urllib.request.urlretrieve(url, file_path)
+
 
 # 代码清单2-1 通过Python读取短篇小说THE VERDICT作为文本样本
 with open ("the-verdict.txt", "r", encoding="utf-8") as f:
@@ -16,5 +19,18 @@ with open ("the-verdict.txt", "r", encoding="utf-8") as f:
 preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
 # 去除空白符
 preprocessed = [item.strip() for item in preprocessed if item.strip()]
-print("Total number of tokens:", len(preprocessed))
-print(preprocessed[:30])
+# print("Total number of tokens:", len(preprocessed))
+# print(preprocessed[:30])
+
+# 确定词汇表的大小
+all_words = sorted(set(preprocessed))
+# print("Total number of unique words:", len(all_words))
+
+
+# 代码清单2-2 创建词汇表
+vocab = {token:integer for integer, token in enumerate(all_words)}
+# for i, item in enumerate(vocab.items()):
+#     print(item)
+#     if i >= 50:
+#         break
+
