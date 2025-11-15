e6765614e5da684edb39a5b90daea2abae42be19
HaPpY <930333105@qq.com>
2025-11-16 00:04:53 +0800

    feat(U4): 添加 GELU 激活函数实现并注释原有 LayerNorm 测试代码
    - 添加 GELU 激活函数类实现
    - 注释掉原有的 LayerNorm 测试代码段
    - 设置环境变量解决多线程库冲突问题

--- U4/01_main-chapter-code/U4_All-the-Codes.py
+++ U4/01_main-chapter-code/U4_All-the-Codes.py
@@ -1,3 +1,6 @@
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='TRUE'
+
 import torch
 import torch.nn as nn
 import tiktoken
@@ -121,10 +124,39 @@ class LayerNorm(nn.Module):
         return self.scale * norm_x + self.shift
 
 
-batch_example = torch.randn(2, 5)
-ln = LayerNorm(emb_dim=5)
-out_ln = ln(batch_example)
-mean = out_ln.mean(dim = -1, keepdim = True)
-var = out_ln.var(dim = -1, unbiased = False, keepdim = True)
-print("Mean:\n", mean)
-print("Variance:\n", var)
+# batch_example = torch.randn(2, 5)
+# ln = LayerNorm(emb_dim=5)
+# out_ln = ln(batch_example)
+# mean = out_ln.mean(dim = -1, keepdim = True)
+# var = out_ln.var(dim = -1, unbiased = False, keepdim = True)
+# print("Mean:\n", mean)
+# print("Variance:\n", var)
+
+
+# 代码清单 4-3 GELU激活函数的实现
+class GELU(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return 0.5 * x * (1 + torch.tanh(
+            torch.sqrt(torch.tensor(2.0 / torch.pi)) * (x + 0.044715 * torch.pow(x, 3))
+        ))
+
+
+# # 为了直观地比较 GELU 函数与 RELU 函数，可以将它们并排绘制出来
+# import matplotlib.pyplot as plt
+# gelu , relu = GELU(), nn.ReLU()
+# # 在 -3 和 3 之间创建 100 个样本数据点
+# x = torch.linspace(-3, 3, 100)
+# y_gelu, y_relu = gelu(x), relu(x)
+# plt.figure(figsize = (8, 3))
+# for i, (y, label) in enumerate(zip([ y_gelu, y_relu], ["GELU", "ReLU"]), 1):
+#     plt.subplot(1, 2, i)
+#     plt.plot(x, y)
+#     plt.title(f"{label} activation function")
+#     plt.xlabel("x")
+#     plt.ylabel(f"{label}(x)")
+#     plt.grid(True)
+# plt.tight_layout()
+# plt.show()
