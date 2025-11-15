2267c6f3046461f7c49fec6498fdaabe98729684
HaPpY <930333105@qq.com>
2025-11-15 23:22:02 +0800

    refactor(U4):重命名并更新代码文件以包含所有章节代码- 将文件 U4.py 重命名为 U4_All-the-Codes.py - 注释掉原有的打印 batch 的语句 - 添加模型初始化和前向传播逻辑- 打印模型输出的形状和具体值

--- U4/01_main-chapter-code/U4_All-the-Codes.py
+++ U4/01_main-chapter-code/U4_All-the-Codes.py
@@ -74,4 +74,10 @@ txt2 = "Every day holds a"
 batch.append(torch.tensor(tokenizer.encode(txt1)))
 batch.append(torch.tensor(tokenizer.encode(txt2)))
 batch = torch.stack(batch, dim = 0)
-print(batch)
+# print(batch)
+
+torch.manual_seed(123)
+model = DummyGPTModel(GPT_CONFIG_124M)
+logits = model(batch)
+print("Output shape:", logits.shape)
+print(logits)
