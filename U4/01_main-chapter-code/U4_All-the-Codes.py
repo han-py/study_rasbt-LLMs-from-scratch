14a23cf82397cd3c116913ec71da414319a994b7
HaPpY <930333105@qq.com>
2025-11-15 23:50:22 +0800

    fix(U4_All-the-Codes.py): 修复LayerNorm中的拼写错误并更新参数名称- 将torch.onse更正为torch.ones- 将unbias参数更正为unbiased - 添加LayerNorm的测试代码以验证实现 - 打印归一化后的均值和方差以确认功能正确性

--- U4/01_main-chapter-code/U4_All-the-Codes.py
+++ U4/01_main-chapter-code/U4_All-the-Codes.py
@@ -111,11 +111,20 @@ class LayerNorm(nn.Module):
     def __init__(self, emb_dim):
         super().__init__()
         self.eps = 1e-5
-        self.scale = nn.Parameter(torch.onse(emb_dim))
+        self.scale = nn.Parameter(torch.ones(emb_dim))
         self.shift = nn.Parameter(torch.zeros(emb_dim))
 
     def forward(self, x):
         mean = x.mean(dim = -1, keepdim = True)
-        var = x.var(dim = -1, keepdim = True, unbias =  False)
+        var = x.var(dim = -1, keepdim = True, unbiased = False)
         norm_x = (x - mean) / torch.sqrt(var + self.eps)
         return self.scale * norm_x + self.shift
+
+
+batch_example = torch.randn(2, 5)
+ln = LayerNorm(emb_dim=5)
+out_ln = ln(batch_example)
+mean = out_ln.mean(dim = -1, keepdim = True)
+var = out_ln.var(dim = -1, unbiased = False, keepdim = True)
+print("Mean:\n", mean)
+print("Variance:\n", var)
