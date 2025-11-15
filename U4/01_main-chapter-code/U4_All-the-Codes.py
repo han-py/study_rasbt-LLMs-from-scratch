373abe0100f3e2c6109cb23c8f079841ca3b9607
HaPpY <930333105@qq.com>
2025-11-15 23:43:14 +0800

    feat(nn): 实现层归一化类
    - 添加 LayerNorm 类定义- 初始化 epsilon、scale 和 shift 参数
    - 实现前向传播逻辑
    - 计算均值和方差- 应用归一化公式- 返回缩放和平移后的结果

--- U4/01_main-chapter-code/U4_All-the-Codes.py
+++ U4/01_main-chapter-code/U4_All-the-Codes.py
@@ -104,3 +104,18 @@ class DummyLayerNorm(nn.Module):
 # torch.set_printoptions(sci_mode=False)
 # print("Mean:\n", mean)
 # print("Variance:\n", var)
+
+
+# 代码清单 4-2 层归一化类
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
+        super().__init__()
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.onse(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+
+    def forward(self, x):
+        mean = x.mean(dim = -1, keepdim = True)
+        var = x.var(dim = -1, keepdim = True, unbias =  False)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift
