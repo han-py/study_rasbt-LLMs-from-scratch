4273b5f9ea12e650de502cb085bdb54e4152cf1c
HaPpY <930333105@qq.com>
2026-05-10 15:44:13 +0800

    feat(data): 实现数据平衡处理功能
    - 移除CSV读取代码注释并启用实际数据加载功能
    - 添加create_balanced_dataset函数实现数据平衡处理
    - 实现随机采样机制确保垃圾信息与非垃圾信息样本数量一致
    - 添加数据集合并逻辑创建平衡数据集
    - 实现标签映射将文本标签转换为数值形式便于模型训练

--- U6/01_main-chapter-code/U6_All-the-Codes.py
+++ U6/01_main-chapter-code/U6_All-the-Codes.py
@@ -34,13 +34,29 @@ download_and_unzip_spam_data(url, zip_path, extracted_path, data_file_path)
 
 
 import pandas as pd
-# df = pd.read_csv(
-#     data_file_path,
-#     sep="\t",
-#     header=None,
-#     names=["Label", "Text"]
-# )
+df = pd.read_csv(
+    data_file_path,
+    sep="\t",
+    header=None,
+    names=["Label", "Text"]
+)
 # print(df)
 # print(df.Label.value_counts())
 
 
+# 代码清单 6-2 创建一个平衡的数据集
+def create_balanced_dataset(df):
+    num_spam =df[df["Label"] == "spam"].shape[0] # 统计“垃圾信息”的样本数量
+    ham_subset = df[df["Label"] == "ham"].sample(
+        num_spam,
+        random_state=123
+    ) # 随机采样“非垃圾消息”，使其数量与“垃圾消息”一致
+    balanced_df = pd.concat([
+        ham_subset, df[df["Label"] == "spam"]
+    ]) # 将“垃圾消息”与采样后的“非垃圾消息”组合，构成平衡数据集
+    return balanced_df
+
+balanced_df = create_balanced_dataset(df)
+# print(balanced_df["Label"].value_counts())
+
+balanced_df["Label"] = balanced_df["Label"].map({"ham": 0, "spam": 1}) # 将标签转换为数值形式，方便后续模型训练
