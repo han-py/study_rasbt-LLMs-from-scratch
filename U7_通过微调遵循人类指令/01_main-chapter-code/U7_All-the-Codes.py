fd24e67758a7b18a007dff47302c86475a0e540a
HaPpY <930333105@qq.com>
2026-05-13 21:02:07 +0800

    feat(dataset): 添加数据集划分功能
    - 注释掉原有的模型输入打印功能
    - 实现训练集、测试集和验证集的数据划分逻辑
    - 按照85%训练集、10%测试集、5%验证集的比例进行划分
    - 添加数据集长度的打印输出功能

--- U7_通过微调遵循人类指令/01_main-chapter-code/U7_All-the-Codes.py
+++ U7_通过微调遵循人类指令/01_main-chapter-code/U7_All-the-Codes.py
@@ -54,4 +54,18 @@ The correct spelling is 'Occasion.'
 
 model_input = format_input(data[999])
 desired_response = f"\n\n### Response:\n{data[999]['output']}"
-print(model_input + desired_response)
+# print(model_input + desired_response)
+
+
+# 代码清单 7-3 划分数据集
+train_portion = int(len(data) * 0.85) # 使用85%的数据作为训练集
+test_portion = int(len(data) * 0.1) # 使用10%的数据作为测试集
+val_portion = len(data) - train_portion - test_portion # 使用剩下的5%的数据作为验证集
+
+train_data = data[:train_portion]
+test_data = data[train_portion:train_portion + test_portion]
+val_data = data[train_portion + test_portion:]
+
+print("Training set length:", len(train_data))
+print("Validation set length:", len(val_data))
+print("Test set length:", len(test_data))
